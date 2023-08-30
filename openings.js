360d838ee173c0492b86adaac555b9475d8cd1d6
hardly0k <henri.v.kulmala@gmail.com>
2023-08-30 19:36:26 +0300

    update
    Minor tweaks and updated the ruy lopez opening with a slide like element

--- openings.js
+++ openings.js
@@ -97,3 +97,27 @@ BtnB.addEventListener("click", function(e){
     
 });
 
+let slideIndex = 0;
+showSlide(slideIndex);
+
+function changeSlide(n) {
+    showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+    let slides = document.getElementsByClassName("slide");
+    if (n >= slides.length) {
+        slideIndex = 0;
+    } else if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
+
+    
+    
+    
+
