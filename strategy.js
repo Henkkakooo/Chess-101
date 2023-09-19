cf65854b03844e431bd955acce475288f66597a8
hardly0k <henri.v.kulmala@gmail.com>
2023-09-19 16:12:34 +0300

    Update on strategies
    - added terminology "page"
    - added tips to strategies
    - making a terminology game

--- strategy.js
+++ strategy.js
@@ -4,6 +4,41 @@ const endGameBtn = document.getElementById("endGameBtn");
 const openingForm = document.getElementById("openingForm");
 const middleForm = document.getElementById("middleForm");
 const endGameForm = document.getElementById("endGameForm");
+const termsYes = document.getElementById("terminologyYes");
+const termsNo = document.getElementById("terminologyNo");
+const termsForm = document.getElementById("termsForm");
+const yesOrNo = document.getElementById("yesOrNo");
+const questionForm = document.getElementById("yesOrNo");
+const strategyForm = document.getElementById("strategy");
+const showTerms = document.getElementById("showTerms");
+const termsBack = document.getElementById("termsBack");
+
+
+termsBack.addEventListener("click", function(e){
+    e.preventDefault();
+    strategyForm.style.display = strategyForm.style.display === "none"? "block" : "none";
+    termsForm.style.display = "none";
+});
+
+termsYes.addEventListener("click", function(e){
+    e.preventDefault();
+    termsForm.style.display = termsForm.style.display === "none"? "block" : "none";
+    yesOrNo.style.display = "none";
+
+});
+
+termsNo.addEventListener("click", function(e){
+    e.preventDefault();
+    strategyForm.style.display = strategyForm.style.display === "none"? "block" : "none";
+    
+    yesOrNo.style.display = "none";
+});
+
+showTerms.addEventListener("click", function(e){
+    e.preventDefault();
+    termsForm.style.display = termsForm.style.display === "none"? "block" : "none";
+    strategyForm.style.display = "none";
+});
 
 openingBtn.addEventListener("click", function(e){
     e.preventDefault();
