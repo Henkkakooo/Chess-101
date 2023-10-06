c37348a9644dfeb71c2b7ddf65b8d6e48fc6af84
hardly0k <henri.v.kulmala@gmail.com>
2023-10-06 18:31:19 +0300

    update on heurestics

---
+++ index.js
@@ -0,0 +1,34 @@
+function expand(h3Element) {
+    
+    var h3Sibling = h3Element.querySelector('span');
+  
+    if (h3Sibling.style.display === 'none' || h3Sibling.style.display === '') {
+        h3Sibling.style.display = 'inline'; 
+    } else {
+        h3Sibling.style.display = 'none';
+    }
+  }
+
+  const firstContainer = document.getElementById('firstContainer');
+  const secondContainer = document.getElementById('secondContainer');
+
+  firstContainer.addEventListener('mouseover', () =>  {
+    secondContainer.classList.add('blur');
+    firstContainer.classList.add("transform");
+    
+
+  });
+  firstContainer.addEventListener('mouseout', () => {
+
+    secondContainer.classList.remove('blur');
+    firstContainer.classList.remove("transform");
+  });
+
+  secondContainer.addEventListener('mouseover', () => {
+
+    firstContainer.classList.add('blur');
+  });
+
+  secondContainer.addEventListener('mouseout', () => {
+    firstContainer.classList.remove('blur');
+  });
